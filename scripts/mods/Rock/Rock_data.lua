e4908cea5d9bad2e1c11c68034af8b2a24cdb213
Wobin <wobster@gmail.com>
2023-10-07 23:23:29 +1100

    Add BONK

--- scripts/mods/Rock/Rock_data.lua
+++ scripts/mods/Rock/Rock_data.lua
@@ -6,7 +6,16 @@ return {
 	is_togglable = true,
   options = {
 		widgets = {
-		
+      {
+        setting_id = "amusing_bonk",
+        type = "checkbox",
+        default_value = false
+        },
+			{
+				setting_id = "single_bonk_noise",
+				type = "checkbox",
+				default_value = false
+			}
 		}
 	}
 }
