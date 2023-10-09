1b0683e3ec8ba8840ec6cd6606743e31b6327642
xsSplater <xssplater@gmail.com>
2023-10-09 04:39:23 +0300

    RU Rock_localization.lua

--- scripts/mods/Rock/Rock_localization.lua
+++ scripts/mods/Rock/Rock_localization.lua
@@ -1,17 +1,22 @@
 return {
-  mod_name = {
-    en = "Rock",    
-  },
-  mod_description = {
-    en = "Adds some spice to the Ogryn's Big Friendly Rock ability",    
-  },
-  amusing_bonk = {
-    en = "Include entertaining bonk noises on impact"
-  },
-  single_bonk_noise = {
-    en = "Only use a single bonk"
-  },
-  rock_pickup = {
-    en = "Notify when you find a new rock"
-    }
+	mod_name = {
+		en = "Rock",
+		ru = "Камень",
+	},
+	mod_description = {
+		en = "Adds some spice to the Ogryn's Big Friendly Rock ability",
+		ru = "Rock - Добавляет остроты к способности Огрина «Большой дружелюбный камень».",
+	},
+	amusing_bonk = {
+		en = "Include entertaining bonk noises on impact",
+		ru = "Включает забавные звуки при ударе.",
+	},
+	single_bonk_noise = {
+		en = "Only use a single bonk",
+		ru = "Использовать только один звук удара",
+	},
+	rock_pickup = {
+		en = "Notify when you find a new rock",
+		ru = "Сообщать, когда вы находите новый камень",
+	},
 }
