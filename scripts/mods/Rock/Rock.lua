2be18cff1586b24517f8abc65b9bb76bcb231fff
Wobin <wobster@gmail.com>
2024-05-06 23:45:34 +1000

    Replaced custom rock shouts with in-game audio

--- scripts/mods/Rock/Rock.lua
+++ scripts/mods/Rock/Rock.lua
@@ -1,9 +1,9 @@
 --[[
 Title: Rock
 Author: Wobin
-Date: 11/12/2023
+Date: 18/04/2024
 Repository: https://github.com/Wobin/Rock
-Version: 2.1
+Version: 3.0
 ]]--
 
 local CharacterSheet = require("scripts/utilities/character_sheet")
@@ -12,7 +12,6 @@ local Audio
 local audio_files
 local HoldingRock
 local player
-local rockShouts = "noise"
 local friendgryn = "friend"
 local impact = "impact"
 local pickup = "find"
@@ -27,6 +26,9 @@ local class_loadout = {
 mod.weOgryn = function()
     player = Managers.player:local_player(1)  
     if not player then return false end
+    if not mod.ogrynVoice and player:profile().archetype.breed == "ogryn" then
+      mod.ogrynVoice = player:profile().selected_voice
+    end
     return player:profile().archetype.breed == "ogryn"     
 end
 
@@ -39,16 +41,18 @@ end
 mod.friendShout = function (self, attacking_unit)
   local player_manager = Managers.player
 	local players = player_manager:players()  
+  local count = 1
   for i,member in pairs(players) do            
-    if member ~= player and member:is_human_controlled() and member._profile.archetype.breed == "ogryn" and (not attacking_unit or (attacking_unit and member.player_unit ~= attacking_unit)) then          
-          Audio.play_file(audio_files:random(friendgryn), { audio_type = "sfx", adelay = "1200|1200", chorus = "0.6:0.9:50|60:0.4|0.32:0.25|0.4:2|1.3" }, member.player_unit, 0.001)        
+    if member ~= player and member:is_human_controlled() and member._profile.archetype.breed == "ogryn" and (not attacking_unit or (attacking_unit and member.player_unit ~= attacking_unit)) then                  
+          Promise.delay(2 + (0.5 * count)):next(function() Audio.play("loc_"..(member:profile().selected_voice or "ogryn_a") .."__blitz_rock_a_"..string.format("%02d", math.random(1,10)), member.player_unit) end)
+          count = count + 1
     end
   end    
 end
 
 mod.shoutRock = function(self, delta, override)    
-    if (override or HoldingRock) and (delta == nil or delta > 0.1) then             
-       Audio.play_file(audio_files:random(rockShouts), { audio_type = "sfx", adelay = "500|500", chorus = "0.6:0.9:50|60:0.4|0.32:0.25|0.4:2|1.3", })       
+    if (override or HoldingRock) and (delta == nil or delta > 0.1) then                
+       Promise.delay(0.5):next(function() Audio.play("loc_".. mod.ogrynVoice .."__blitz_rock_a_"..string.format("%02d", math.random(1,10))) end)
         if mod:get("friend_ogryn") then          
           mod:friendShout(override)
         end
@@ -82,6 +86,7 @@ end
 mod.on_all_mods_loaded = function()
     Audio = get_mod("Audio")    
     audio_files = Audio.new_files_handler()
+    
     -- Hook the rock tossing --
     mod:hook_require("scripts/extension_systems/weapon/actions/action_throw_grenade", function(altFire)
       mod:hook_safe(altFire, "start", function(self, ...)              
@@ -90,15 +95,17 @@ mod.on_all_mods_loaded = function()
     end)
     
     Audio.hook_sound("wwise/events/weapon/stop_player_combat_weapon_grenader_loop", function(_, _, delta)
-        if mod:weOgryn() and HoldingRock then
+        if mod:weOgryn() and HoldingRock then                  
           HoldingRock = mod:shoutRock(delta)
         end      
       return true
     end)
 
+    Audio.hook_sound("_blitz_rock_a", function() return false end)
+    
      -- Hook when rock regenerates
     Audio.hook_sound("wwise/events/player/play_player_grenade_charge_restored_gen", function(_, _, delta)
-        if mod:weOgryn() and mod:get("rock_pickup") and mod:doIHaveRock() then
+        if mod:weOgryn() and mod:get("rock_pickup") and mod:doIHaveRock() then          
           mod:pickupRock()
         end      
       return true
@@ -121,5 +128,5 @@ mod.on_all_mods_loaded = function()
         end
       end   
     end
-  end)
+  end)  
 end
